5cb0117daf70bca385145a38fc3f6e72672a0c03
jomoal1190 <mongealvarez@gmail.com>
2018-10-17 00:15:16 -0600

    Cambios en rutas securyt

--- src/main/java/com/umg/programacion2/SecurityConfiguration.java
+++ src/main/java/com/umg/programacion2/SecurityConfiguration.java
@@ -66,7 +66,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "../../../css/**", "../../../js/**", "../../../images/**", "../../../fonts/**", "../../../vendor/**");
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/fonts/**", "/vendor/**");
     }
 
 }
